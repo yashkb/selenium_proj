7bca30c1cf694919eeb4430ec3ec0b511663658e
ADMIN <yashkb79@gmail.com>
2023-07-31 23:51:25 +0530

    Jenkins first testing

--- orangeCRM/src/test/java/com/orangeCRM/testCases/TC_Login_001.java
+++ orangeCRM/src/test/java/com/orangeCRM/testCases/TC_Login_001.java
@@ -14,11 +14,13 @@ public class TC_Login_001 extends BaseClass {
 		loginObj  = new Login(driver);
 		driver.get(url);
 		Thread.sleep(3000);
+		System.out.println("hello Yash");
 		String userId = "admin";
 		loginObj.enterUserName(userId);
 		String password = "admin123";
 		loginObj.enterPassword(password);
 		loginObj.clickLogin();
+		
 	}
 	
 	
